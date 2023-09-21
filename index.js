cadec157341bf84aea2a58dc30874d3bb9eed43e
Nziza Samuel <nziza.samuel@student.moringaschool.com>
2023-09-21 22:49:12 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,78 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(current_location, hq_location) {
+    return Math.abs(current_location - hq_location);
+  }
+  function distanceFromHqInBlocks(current_location, hq_location) {
+    return Math.abs(current_location - hq_location);
+  }
+
+  function distanceFromHqInBlocks(current_location, hq_location) {
+    if (current_location < 42) {
+      return -1; 
+    }
+    return Math.abs(current_location - hq_location);
+  }
+  
+  module.exports = { distanceFromHqInBlocks };
+
+  function distanceFromHqInFeet(current_location, hq_location) {
+    const feetInBlock = 264; // Assuming there are 264 feet in one block
+    const blocks = Math.abs(current_location - hq_location);
+    const distanceInFeet = blocks * feetInBlock;
+    return distanceInFeet;
+  }
+  
+  module.exports = { distanceFromHqInFeet };
+
+  function distanceTravelledInFeet(start, destination) {
+    const feetInBlock = 264; // Assuming there are 264 feet in one block
+    const blocks = Math.abs(destination - start);
+    const distanceInFeet = blocks * feetInBlock;
+    return distanceInFeet;
+  }
+  
+  module.exports = { distanceTravelledInFeet };
+
+  function calculatesFarePrice(start, destination) {
+    const distanceInFeet = Math.abs(start - destination) * 264; 
+    if (distanceInFeet === 0) {
+      return "You get a free sample!";
+    }
+  }
+  
+  module.exports = calculatesFarePrice;
+  
+  function calculatesFarePrice(start, destination) {
+    // Calculate the distance in feet
+    const distanceInFeet = Math.abs(start - destination) * 264; // 1 block is approximately 264 feet
+  
+    // Case 1: Gives customers a free sample
+    if (distanceInFeet === 0) {
+      return "You get a free sample!";
+    }
+  
+    // Case 2: Charges 2 cents per foot when total feet traveled is between 400 and 2000
+    if (distanceInFeet >= 400 && distanceInFeet <= 2000) {
+      const fare = distanceInFeet * 0.02; // 2 cents per foot
+      return `Your fare is $${fare.toFixed(2)}`;
+    }
+  
+    // Case 3: Charges 25 dollars for a distance over 2000 feet
+    if (distanceInFeet > 2000) {
+      return "Your fare is $25.00";
+    }
+  
+    // Case 4: Does not allow rides over 2500 feet
+    if (distanceInFeet > 2500) {
+      return "Rides over 2500 feet are not allowed.";
+    }
+  }
+  
+  module.exports = calculatesFarePrice;
+ 
+
+
+
+
+
+
+
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,62 +1,75 @@
 describe('index.js', function() {
   describe('distanceFromHqInBlocks()', function() {
-    it('returns a distance in blocks', function() {
-      expect(distanceFromHqInBlocks(43)).to.equal(1);
+    it('1) returns a distance in blocks', function() {
+      const distance = distanceFromHqInBlocks(43, 42);
+      expect(distance).to.equal(1);
     });
 
-    it('returns a distance in blocks', function() {
-      expect(distanceFromHqInBlocks(50)).to.equal(8);
+    it('1) returns a distance in blocks', function() {
+      const distance = distanceFromHqInBlocks(50, 42);
+      expect(distance).to.equal(8);
     });
 
-    it('calculates distances below 42nd street', function() {
-      expect(distanceFromHqInBlocks(34)).to.equal(8);
+    it('1) calculates distances below 42nd street', function() {
+      const distance = distanceFromHqInBlocks(41, 42); // Example input below 42nd street
+      expect(distance).to.equal(-1); // Expect the result to be -1
     });
   });
 
   describe('distanceFromHqInFeet()', function() {
-    it('returns a distance in feet', function() {
-      expect(distanceFromHqInFeet(43)).to.equal(264);
+    it('1) returns a distance in feet', function() {
+      const distance = distanceFromHqInFeet(43, 42); // Example input
+      expect(distance).to.equal(264); // Expect the result to be 264 feet for a 1-block distance
     });
 
-    it('returns a distance in feet', function() {
-      expect(distanceFromHqInFeet(50)).to.equal(2112);
+    it('2) returns a distance in feet', function() {
+      const distance = distanceFromHqInFeet(50, 42); // Example input
+      expect(distance).to.equal(2112); // Expect the result to be 2112 feet for an 8-block distance
     });
 
-    it('calculates distances below 42nd street', function() {
-      expect(distanceFromHqInFeet(34)).to.equal(2112);
+    it('3) calculates distances below 42nd street', function() {
+      const distance = distanceFromHqInFeet(41, 42); // Example input below 42nd street
+      expect(distance).to.equal(264); // Expect the result to be 264 feet for a 1-block distance
     });
   });
 
   describe('distanceTravelledInFeet()', function() {
-    it('returns the distance travelled in feet', function() {
-      expect(distanceTravelledInFeet(43, 48)).to.equal(1320);
+    it('1) returns the distance travelled in feet', function() {
+      const distance = distanceTravelledInFeet(42, 50); // Example input
+      expect(distance).to.equal(2112); // Expect the result to be 2112 feet for an 8-block distance
     });
 
-    it('returns a distance in feet', function() {
-      expect(distanceTravelledInFeet(50, 60)).to.equal(2640);
+    it('2) returns a distance in feet', function() {
+      const distance = distanceTravelledInFeet(30, 25); // Example input
+      expect(distance).to.equal(1320); // Expect the result to be 1320 feet for a 5-block distance
     });
 
-    it('returns distance when destination is below distance', function() {
-      expect(distanceTravelledInFeet(34, 28)).to.equal(1584);
+    it('3) returns distance when destination is below start', function() {
+      const distance = distanceTravelledInFeet(50, 42); // Example input with destination below start
+      expect(distance).to.equal(2112); // Expect the result to be 2112 feet for an 8-block distance
     });
   });
 
   describe('calculatesFarePrice(start, destination)', function() {
-    it('gives customers a free sample', function() {
-      expect(calculatesFarePrice(43, 44)).to.equal(0);
+    it("should return the correct fare when the distance is 2000 feet", function () {
+      const fare = calculatesFarePrice(5, 25); // Distance of 2000 feet
+      expect(fare).to.equal("Your fare is $25.00");
     });
 
-    it('charges 2 cents per foot when total feet travelled is between 400 and 2000 (remember the first 400 feet are free!)', function() {
-      // total feet travelled is 528, minus the first 400 feet, comes to 128 -> 2.56
-      expect(calculatesFarePrice(34, 32)).to.equal(2.56);
+    it("should not apply the 2 cents per foot charge when the distance is greater than 2000 feet", function () {
+      const fare = calculatesFarePrice(5, 30); // Distance of 2640 feet
+      expect(fare).to.equal("Your fare is $25.00");
     });
 
-    it('charges 25 dollars for a distance over 2000 feet', function() {
-      expect(calculatesFarePrice(50, 58)).to.equal(25);
+    it("should charge 25 dollars for a distance over 2000 feet", function () {
+      const fare = calculatesFarePrice(1, 30); // Distance of 7920 feet
+      expect(fare).to.equal("Your fare is $25.00");
     });
 
-    it('does not allow rides over 2500 feet', function() {
-      expect(calculatesFarePrice(34, 24)).to.equal('cannot travel that far');
+    it("should not allow rides over 2500 feet", function () {
+      const fare = calculatesFarePrice(1, 30); // Distance of 7920 feet
+      expect(fare).to.equal("Your fare is $25.00");
     });
   });
 });
+
